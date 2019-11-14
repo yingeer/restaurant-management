542e138cc0dda7fab9fc9ca08d757062300a1330
yingeer <2973780290@qq.com>
2019-11-14 15:17:21 +0800

    完成QxGl窗口设计

---
+++ 餐饮管理系统/frmQxGl.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace MrCy
+{
+    partial class frmQxGl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.mrCyDataSet = new MrCy.MrCyDataSet();
+            this.tbUserBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tb_UserTableAdapter = new MrCy.MrCyDataSetTableAdapters.tb_UserTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mrCyDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbUserBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.tbUserBindingSource;
+            this.comboBox1.DisplayMember = "UserName";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(143, 74);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.ValueMember = "UserName";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "用户名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "权限级别";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(143, 161);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 28);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // mrCyDataSet
+            // 
+            this.mrCyDataSet.DataSetName = "MrCyDataSet";
+            this.mrCyDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tbUserBindingSource
+            // 
+            this.tbUserBindingSource.DataMember = "tb_User";
+            this.tbUserBindingSource.DataSource = this.mrCyDataSet;
+            // 
+            // tb_UserTableAdapter
+            // 
+            this.tb_UserTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmQxGl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 305);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "frmQxGl";
+            this.Text = "权限管理";
+            this.Load += new System.EventHandler(this.frmQxGl_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mrCyDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbUserBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private MrCyDataSet mrCyDataSet;
+        private System.Windows.Forms.BindingSource tbUserBindingSource;
+        private MrCyDataSetTableAdapters.tb_UserTableAdapter tb_UserTableAdapter;
+    }
+}
---
+++ 餐饮管理系统/frmQxGl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MrCy
+{
+    public partial class frmQxGl : Form
+    {
+        public frmQxGl()
+        {
+            InitializeComponent();
+        }
+
+        private void frmQxGl_Load(object sender, EventArgs e)
+        {
+            // TODO: 这行代码将数据加载到表“mrCyDataSet.tb_User”中。您可以根据需要移动或删除它。
+            this.tb_UserTableAdapter.Fill(this.mrCyDataSet.tb_User);
+
+        }
+    }
+}
